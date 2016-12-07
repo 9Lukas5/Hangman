d421a2ab8196498555c5ad85fb994405f6284a29
9Lukas5 <Lukas.Wiest@kies-gmbh.de>
2016-12-07 17:24:45 +0100

    project: init

---
+++ src/de/hft_stuttgart/wiest_wolf/Hangman.java
@@ -0,0 +1,16 @@
+package de.hft_stuttgart.wiest_wolf;
+
+/**
+ *
+ * @author Lukas Wiest
+ * @author Erik Wolf
+ */
+public class Hangman
+{
+    
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
